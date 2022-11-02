df89fccaad71e81c8b55bff165604d25fe7c472b
Anastasiya_Fernandez <anastasiya.ustych@gmail.com>
2022-11-02 21:52:46 +0100

    first commit2

---
+++ demo.php
@@ -0,0 +1,9 @@
+<?php
+$bessie = new  Vache ( 'Hello, Farm!' );
+$bessie -> setEyes ( 'oO' )
+    -> setLangue ( 'U' )
+    -> setPoop ( '@@@' )
+    -> setUdder ( 'W' );
+echo  $bessie ;
+
+?>
--- index.php
+++ index.php
@@ -1,22 +1,14 @@
 <?php
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-$ bessie = new  Vache ( 'Hello, Farm!' ) { 
 
-// stocke la sortie dans une variable 
-$ output = $ bessie -> say ();
-echo  $ sortie ;
-}
+$bessie = new  Vache ( 'Hello, Farm!' ) ;
+
+$output = $bessie -> say ();
+echo  $sortie ;
+
 // ou juste faire écho à l'objet pour une sortie directe 
-echo  $ bessie ;
+echo  $bessie ;
 
 // stocke la sortie dans une variable 
 
-
- 
-$ bessie = new  Vache ( 'Hello, Farm!' );
-$ bessie -> setEyes ( 'oO' )
-    -> setLangue ( 'U' )
-    -> setPoop ( '@@@' )
-    -> setUdder ( 'W' );
-echo  $ bessie ;
 ?>
